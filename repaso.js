26d1deda0d3cb081a2f0f2daf11e1057a593c304
sergio tello <alan260398@hotmail.com>
2019-10-22 20:17:43 -0500

    flotante y booleano

--- repaso.js
+++ repaso.js
@@ -1,4 +1,8 @@
 // variables 
 const string = 'Hola mundo'; //const --> nunca va a cambiar su valor 
 
-let entero = 63; //solo se puede usar en el scope creado 
+let entero = 63; // let --> solo se puede usar en el scope creado 
+
+var flotante = 7485.38; //var --> se usa de manera global 
+
+const booleano = false; 
